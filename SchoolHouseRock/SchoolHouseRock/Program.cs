c4104984e6892843e83ba9cccdbe39b5082c1856
blaise594 <danielbrogers594@gmail.com>
2017-04-04 18:52:45 -0400

    first commit

---
+++ SchoolHouseRock/SchoolHouseRock/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolHouseRock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString =
+                @"Server=localhost\SQLEXPRESS;Database=DotNetUniversity;Trusted_Connection=True;";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                Console.WriteLine("Connected");
+                connection.Close();
+            }
+        }
+    }
+}
